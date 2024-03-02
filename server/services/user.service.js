4639002df2cfd61539a436c98e7c14fd37b523df
Mithilesh-zenmonk1151 <mithilesh.1151@zenmonk.tech>
2024-03-02 11:47:06 +0530

    comment

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -52,26 +52,3 @@ exports.getUser = async (payload) => {
   }
 };
 
-// onsole.log("hjdfv" ,req.params)
-//         console.log("file" ,req.file)
-//         const { id } = req.params;
-//     const { username, phone, website, title, industry, summary } = req.body;
-//     if (req.body.address) {
-//         var { street, state, city, pincode, country } = req.body.address;
-//     }
-//     const image = req.file.path;
-//         const user = await UsersModel.findByIdAndUpdate(id, {
-//             username: username,
-//             address: { street, state, city, pincode, country },
-//             phone: phone,
-//             website: website,
-//             image: image,
-//             title: title,
-//             summary: summary,
-//             industry: industry
-//         },{new:true});
-//         if (!user) {
-//             throw new CustomError('User not updated' , 404)
-//         } else {
-//             return user
-//         }
